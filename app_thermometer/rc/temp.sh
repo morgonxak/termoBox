20a9ce4e3a02ca325f7cd556ef877d870761f324
Dima <cmit.dima@gmail.com>
2020-09-16 11:35:23 +0700

    init

--- app_thermometer/rc/temp.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-sudo modprobe w1-gpio
-sudo modprobe w1-therm
-roomtemp=$(cat /sys/bus/w1/devices/28-0417720ca4ff/w1_slave | grep  -E -o ".{0,0}t=.{0,5}" | cut -c 3-)
-echo "$roomtemp"
--- expiriments/test_subprocess.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-import cv2
-
-path_LCD = r'./home/pi/project/libseek-thermal/build/examples/seek_test'
-p = subprocess.Popen("{}".format(path_LCD), shell=True, stdout=subprocess.PIPE)
-while 1:
-    print("t= ",p.stdout.read())
