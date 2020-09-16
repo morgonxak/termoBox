1bbc69cd39a2b6160cc66493ac2977abf34c10b1
Dima <cmit.dima@gmail.com>
2020-09-16 13:37:20 +0700

    init_2

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 ## termoBox 09.09.2020
 Данный проект предназначен для измерения температуры с тепловизора и с помощью пирометра 
 Оборудование:
-
+## Зависимости:
+0. https://github.com/adafruit/Adafruit_CircuitPython_AMG88xx.git
+pip
 **mlx90614** - безконтактный датчик температуры
 ## Установка:
 0. Создать виртуальное окружения python3 -m venv termoBox_9_9_20, активировать source termoBox_9_9_20/bin/activate
 0. pip install -r requirements.txt
 0. Запуск: pyhton run.py
+
+## Обновление 
 
 ## Схема подключения оборудования
 ![alt text](https://github.com/morgonxak/termoBox/blob/9_9_20/app_thermometer/rc/connection_diagram.png)
--- app_thermometer/moduls/measurements/amg88.py
+++ app_thermometer/moduls/measurements/amg88.py
@@ -2,21 +2,58 @@ import time
 import busio
 import board
 import adafruit_amg88xx
+import numpy
 
-i2c = busio.I2C(board.SCL, board.SDA)
-amg = adafruit_amg88xx.AMG88XX(i2c)
+class amg88:
+    def __init__(self):
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.amg = adafruit_amg88xx.AMG88XX(self.i2c)
 
-def processing(temp):
-    if temp <= 32.5:
-        temp = temp + 4.3 + abs(32.5 - temp) / 1.23076923
-    else:
-        temp = temp + 4.3
 
-    return temp
+    def getMaxrix(self):
+        '''
+        Получает матрицу с непересчитанными температурами
+        :return:
+        '''
+        return self.amg.pixels
 
-while True:
-    for row in amg.pixels:
-        for temp in row:
-            print(["{0:.1f}".format(processing(temp)) for temp in row])
-    print("\n")
-    time.sleep(1)
+    def getMatrix_recalc(self):
+        '''
+        Пересчитываем всю матрицу с температурами
+        :return:
+        '''
+        matrix_temp = numpy.copy(self.getMaxrix())
+
+        for row_count,  row in enumerate(matrix_temp):
+            for col_count,  temp in enumerate(row):
+                matrix_temp[row_count, col_count] = self.recalculation_of_temperatures(temp)
+
+        return matrix_temp
+
+    def recalculation_of_temperatures(self, temp):
+        '''
+        пересчет температур
+        :param temp:
+        :return:
+        '''
+        if temp <= 32.5:
+            temp = temp + 4.3 + abs(32.5 - temp) / 1.23076923
+        else:
+            temp = temp + 4.3
+        return temp
+
+    def test_1(self):
+        '''
+
+        :return:
+        '''
+
+        while True:
+            for row in self.getMatrix_recalc():
+                print(["{0:.1f}".format(temp) for temp in row])
+            print("\n")
+            time.sleep(1)
+
+if __name__ == '__main__':
+    test = amg88()
+    test.test_1()
