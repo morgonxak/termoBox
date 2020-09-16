1460319129df671c6c04109013168e55c56d6d8e
Dima <cmit.dima@gmail.com>
2020-09-16 12:01:33 +0700

    init_2

--- app_thermometer/moduls/measurements/amg88.py
+++ app_thermometer/moduls/measurements/amg88.py
@@ -6,15 +6,17 @@ import adafruit_amg88xx
 i2c = busio.I2C(board.SCL, board.SDA)
 amg = adafruit_amg88xx.AMG88XX(i2c)
 
+def processing(temp):
+    if temp <= 32.5:
+        temp = temp + 4.3 + abs(32.5 - temp) / 1.23076923
+    else:
+        temp = temp + 4.3
+
+    return temp
+
 while True:
     for row in amg.pixels:
-        # Pad to 1 decimal place
-        # print(["{0:.1f}".format(temp) ])
         for temp in row:
-            if temp <= 32.5:
-                temp = temp + 4.3 + abs(32.5 - temp) / 1.23076923
-            else:
-                temp = temp + 4.3
-            print("{0:.1f}".format(temp))
+            print(["{0:.1f}".format(processing(temp)) for temp in row])
     print("\n")
     time.sleep(1)
